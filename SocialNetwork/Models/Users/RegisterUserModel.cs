eb258a6a638af91cf36c5631697d05069a20e5b5
VasiliyKolihalov <93120654+VasiliyKolihalov@users.noreply.github.com>
2022-07-06 22:47:39 +0300

    Add validation for password (#12)

--- SocialNetwork/Models/Users/RegisterUserModel.cs
+++ SocialNetwork/Models/Users/RegisterUserModel.cs
@@ -7,5 +7,5 @@ public class RegisterUserModel
     [Required] [MaxLength(50)] public string FirstName { get; set; }
     [Required] [MaxLength(50)] public string SecondName { get; set; }
     [EmailAddress] [MaxLength(100)]public string Email { get; set; }
-    [Required] public string Password { get; set; }
+    [Required] [MinLength(8)] public string Password { get; set; }
 }
